0f29607e2a4577d44fa0a9b7993c51073ba4fc60
saharapalomino <saharapc@hotmail.com>
2026-05-09 14:47:58 -0500

    se creo archivo en notebook consumo_usuario para cargar datos,pendiente post de usuarios

---
+++ notebook/consumo_usuario.py
@@ -0,0 +1,17 @@
+import requests
+def consumir_api_tabla_usuarios():
+    
+
+    #1. escribir la Url del servicio que quiero consumir
+    url="http://localhost:8081/api/usuarios"
+
+    #2. utilizar request de python para ir al API
+    respuesta=requests.get(url)
+
+    #3. Verifico la respuesta
+    respuesta.raise_for_status()
+
+    #4. verifico el formato de los datos recibidos
+    datos=respuesta.json()
+
+    return datos 
